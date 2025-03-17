188ce54e67b8a15ffaa677be817eacae55d233e7
mukesh2309 <shelkemukesh6@gmail.com>
2025-03-18 01:11:13 +0530

    fix

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -3,6 +3,8 @@ const assets = {
   whiteCar: require('./images/whitecar.png'),
   overlayBg: require('./images/overlay-bg.png'),
   logo: require('./images/logo.png'),
+  eye: require('./images/eye.png'),
+  logo_black: require('./images/logo_black.png'),
 };
 
 export default assets;
