f49d07ea232051ff3296d0b71f83ab3d47aa45cc
mukesh2309 <shelkemukesh6@gmail.com>
2025-04-01 00:39:01 +0530

    updated code

--- app/screens/search/view/filter.view.tsx
+++ app/screens/search/view/filter.view.tsx
@@ -16,12 +16,13 @@ import {createStyles} from '../search.styles';
 import InputComponent from '../../../components/input/component';
 import {data, FuelType, rentalData, sitingCapacity} from './filter.data';
 import Button from '../../../components/button /component';
+import DateComponent from '../../../components/date/component';
 
 const FilterView = ({visible, setVisible}: IFilterProps) => {
   const [value, setValue] = useState(0);
   const [min, setMin] = useState(0);
   const [max, setMax] = useState(100);
-
+  const [showDatePicker, setShowDatePicker] = useState(false);
   const styles = createStyles();
 
   return (
@@ -91,7 +92,11 @@ const FilterView = ({visible, setVisible}: IFilterProps) => {
             {renderMarginTop(8)}
             <View style={styles.frsb}>
               <Text style={styles.placeHolder}>Pick up and Drop Date</Text>
-              <Text style={styles.placeHolder}>05 June 2025</Text>
+              <Text
+                onPress={() => setShowDatePicker(true)}
+                style={styles.placeHolder}>
+                05 June 2025
+              </Text>
             </View>
             {renderMarginTop(8)}
             <InputComponent
@@ -130,6 +135,10 @@ const FilterView = ({visible, setVisible}: IFilterProps) => {
             {renderMarginBottom(16)}
           </View>
         </ScrollView>
+        <DateComponent
+          visible={showDatePicker}
+          setVisible={setShowDatePicker}
+        />
       </View>
     </BottomSheet>
   );
